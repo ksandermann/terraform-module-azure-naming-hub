e8fb3d838ffaf8f37b19107b1854d00fb7f17bac
root <kevin.sandermann@gmail.com>
2020-04-27 08:20:44 +0000

    update

--- main.tf
+++ main.tf
@@ -12,9 +12,19 @@ locals {
     main = format("%s%s", module.naming-root.resource_basenames["virtual_network"], "HUB001")
   }
 
+  ddos_protection_plan_names = {
+    main = format("%s%s", module.naming-root.resource_basenames["ddos_protection_plan"], "HUB001")
+  }
+
   subnet_names = {
-    gateway         = format("%s%s", local.vnet_names["main"], "GTW001")
-    shared_services = format("%s%s", local.vnet_names["main"], "SSE001")
-    dmz             = format("%s%s", local.vnet_names["main"], "DMZ001")
+    gateway         = format("%s%s", local.vnet_names["main"], "GTWSUB001")
+    shared_services = format("%s%s", local.vnet_names["main"], "SSESUB001")
+    dmz             = format("%s%s", local.vnet_names["main"], "DMZSUB001")
+  }
+
+  nsg_names = {
+    gateway         = format("%s%s", local.subnet_names["gateway"], "NSG001")
+    shared_services = format("%s%s", local.subnet_names["shared_services"], "NSG001")
+    dmz             = format("%s%s", local.subnet_names["dmz"], "NSG001")
   }
 }
