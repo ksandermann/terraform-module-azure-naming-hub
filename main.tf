9c5a207aef1ef4a6f1437f1db48622abb9d92ba1
root <kevin.sandermann@gmail.com>
2020-04-27 07:29:02 +0000

    rg names

--- main.tf
+++ main.tf
@@ -4,7 +4,11 @@ locals {
   location     = var.location
   environment  = var.environment
 
+  resource_group_names = {
+    main = format("%s%s", module.naming-root.resource_basenames["resource_group"], "HUB001")
+  }
+
   vnet_names = {
-    main = format("%s%s", module.naming-root.resource_names["virtual_network"], "HUB001")
+    main = format("%s%s", module.naming-root.resource_basenames["virtual_network"], "HUB001")
   }
 }
