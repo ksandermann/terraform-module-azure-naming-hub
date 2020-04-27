cc0bfad92e3f32c1507bcf3a5d80a960189c4d4e
root <kevin.sandermann@gmail.com>
2020-04-27 07:59:28 +0000

    subnets

--- main.tf
+++ main.tf
@@ -11,4 +11,10 @@ locals {
   vnet_names = {
     main = format("%s%s", module.naming-root.resource_basenames["virtual_network"], "HUB001")
   }
+
+  subnet_names = {
+    gateway         = format("%s%s", local.vnet_names["main"], "GTW001")
+    shared_services = format("%s%s", local.vnet_names["main"], "SSE001")
+    dmz             = format("%s%s", local.vnet_names["main"], "DMZ001")
+  }
 }
